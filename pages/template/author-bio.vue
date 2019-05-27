4e83d0c7be4957ec4e488da375579a38e3c651ee
chinedu <chinedudimonyeka@gmail.com>
2019-05-27 18:07:09 +0100

    WIP: Submission Creat page

---
+++ pages/template/author-bio.vue
@@ -0,0 +1,87 @@
+<template>
+	  <v-container>
+	  	<v-layout row wrap>
+	  	
+	  		<v-flex md12 xs12> 
+                <v-card flat  justify-center>
+	              	<v-container>
+		              	<v-layout grid-list-xs wrap>
+			              	<v-flex xs4 md4>
+			              		<div style="display:flex; justify-content: center">
+					              	<v-avatar
+				                       
+				                       :size="$vuetify.breakpoint.mdAndUp ? '150px' : '80px' "
+				                       class="mt-2 mx-auto"
+				                      >
+				                     	<v-img
+				                     	src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+				                     
+				                     	aspect-ratio="1"
+				                     	>	</v-img>
+				                     </v-avatar>
+			                    </div>
+		                     </v-flex>
+		                     <v-flex md8 xs8>
+			                     <v-card-text>
+			                     	<h3 class="headline"> Emeka Obi</h3>
+					            	<p>
+					                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente officia dignissimos deleniti vel distinctio neque, atque, cum excepturi sit explicabo harum, amet cupiditate minus ad labore ea tempore nisi quod voluptatem unde maxime. 
+					               </p>
+			                     </v-card-text>
+		                   </v-flex>
+		                   <v-flex xs12>
+		                   	  <div style="display:flex; justify-content: flex-end">
+			                    <v-btn outline small class="mt-2">Follow</v-btn>
+		                   	  	
+		                   	  </div>
+		                   </v-flex>
+	               </v-layout>
+               </v-container>
+	              </v-card>
+	  		</v-flex>
+	  		
+	  	</v-layout>
+	   </v-container>
+</template>
+<script>
+
+
+export default {
+
+// middleware: ['middleware'],
+
+layout: 'default',
+
+
+// components: { }, 
+
+
+asyncData(){
+
+ return { }
+},
+
+// async fetch({store,params}){
+    
+//     await store.dispatch("storeNamespace/storeAction")
+        
+// },
+
+computed:{
+   
+   // storeAction(){
+
+   //       return this.$store.getters['storeNamespace/storeGetter']
+   // }
+},
+methods:{
+    
+
+},
+
+}
+</script>
+
+<style lang="stylus">
+
+</style>
---
+++ pages/template/form-author-profile.vue
@@ -0,0 +1,109 @@
+<template>
+  	  <v-container>
+	  	<v-layout row wrap>
+	  	
+	  		<v-flex md12 xs12> 
+                <v-card flat  justify-content-center>
+	              	<v-container>
+		              	<v-layout grid-xs-list wrap>
+			              	<v-flex xs12 md4>
+			              		<div style="display:flex; justify-content: center">
+			              			<v-avatar
+			                       
+			                       :size="$vuetify.breakpoint.mdAndUp ? '150px' : '100px' "
+			                       class="mx-auto"
+			                      >
+			                     	<v-img
+			                     	src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+			                     
+			                     	aspect-ratio="1"
+			                     	>	</v-img>
+			                     </v-avatar>
+			              		</div>
+				              	
+		                     </v-flex>
+		                     <v-flex md8 xs12>
+			                     <v-card-text>
+
+			                     	 <v-text-field label="Pen Name" v-if="editing.pen_name" @blur="editing.pen_name = false" outline v-model="profile.pen_name"></v-text-field>
+
+                                     <h3 class="headline" v-else @click="editing.pen_name = true">{{ profile.pen_name }}</h3>
+
+
+					            	 <v-textarea label="Bio" v-if="editing.bio" @blur="editing.bio = false"outline row="6" v-model="profile.bio">
+					            	 	
+					            	 </v-textarea>
+
+					            	 <p  v-else @click="editing.bio = true">{{ profile.bio }}</p>
+
+					            	 <v-text-field label="Follow Link" outline ></v-text-field>
+					            	 
+					            	
+			                     </v-card-text>
+		                   </v-flex>
+		                   
+	               </v-layout>
+               </v-container>
+	              </v-card>
+	  		</v-flex>
+	  		
+	  	</v-layout>
+	   </v-container>
+</template>
+<script>
+
+
+export default {
+
+// middleware: ['middleware'],
+
+layout: 'default',
+
+data() {
+	return {
+		profile:{ 
+			bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente officia dignissimos deleniti vel distinctio neque, atque, cum excepturi sit explicabo harum, amet cupiditate minus ad labore ea tempore nisi quod voluptatem unde maxime.",
+		      pen_name: "Emeka Obi",
+		     contacts: "https://facebook.com/sdf/"
+		 },
+
+		 editing: {
+		 	bio: false,
+		 	pen_name: false,
+		 	contacts: false,
+		 }
+	}
+},
+
+// components: { }, 
+
+
+asyncData(){
+
+ return { }
+},
+
+// async fetch({store,params}){
+    
+//     await store.dispatch("storeNamespace/storeAction")
+        
+// },
+
+computed:{
+   
+   // storeAction(){
+
+   //       return this.$store.getters['storeNamespace/storeGetter']
+   // }
+},
+methods:{
+    
+
+},
+
+}
+</script>
+
+<style lang="stylus">
+
+</style>
