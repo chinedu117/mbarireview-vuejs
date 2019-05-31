3f0e4792bb42d0956b5041bf06eaa88d46e6a605
chinedu <chinedudimonyeka@gmail.com>
2019-05-31 23:14:25 +0100

    production mode enabled

--- nuxt.config.js
+++ nuxt.config.js
@@ -81,7 +81,7 @@ module.exports = {
   }, 
 
   env: {
-    BASE_URL: process.NODE_ENV == 'production' ? 'http://mbarireview.com.ng/api' : 'http://mbari.test/api',
+    BASE_URL: process.env.NODE_ENV == 'production' ? 'http://mbarireview.com.ng/api' : 'http://mbari.test/api',
     COMPANY_NAME: 'MBARIREVIEW.COM',
     CONTACT_EMAIL: 'contact@mbarireview.com',
     FACEBOOK_CLIENT_ID: '',
