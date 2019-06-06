11a948b651bd17c6cd83dee789ff5f45c4dc7557
chinedu <chinedudimonyeka@gmail.com>
2019-06-06 19:19:32 +0100

    Upload submission enabled

--- pages/dashboard/_author/submission_old.vue
+++ pages/dashboard/_author/submission_old.vue
@@ -1,55 +1,86 @@
 <template>
   <div>
-    <form method="post" enctype="multipart/form-data" @submit.prevent="submit" files="true">
     <v-container>
       <v-layout row wrap>
         <v-flex xs12>
-              <v-btn @click="$router.go(-1)">Back</v-btn>
-
+            <v-btn @click="save">Save</v-btn>
+            <v-btn @click="discard">Discard</v-btn>
              <v-btn @click="submit">Submit</v-btn>
-      
-            <v-btn @click="withdraw">Withdraw</v-btn>
         </v-flex>
       </v-layout>
     </v-container>
-   
-   <div style="width:500px; margin: 0 auto">
-     <v-container>
-      <v-layout row wrap>
-        <v-flex md12 xs12> 
-               <v-select 
+	  <v-container>
+	  	<v-layout row wrap>
+	  	
+	  		<v-flex md12 xs12> 
+                <header>
+
+                	 <span class="grey--text" v-if="!editing.category" @click="editing.category = true"><v-chip>{{ category }}</v-chip> In: {{ edition }}  </span>
+
+                     <v-select 
                       v-model="submission.category_id" 
-                       
+                      v-else 
                       :label="category.title"
                       @change="editing.category = false" 
                       :items="categories"
                       item-value="id"
                       item-text="title"
                       @blur="editing.category = false"
-                      ></v-select> 
-               </v-flex>
-             </v-layout>
-           
-             <v-layout>  
-               <v-flex md12 xs12> 
-                 <v-text-field  v-model="submission.title" outline size="24px"  @blur="editing.title = false"></v-text-field>
-                 </v-flex>
-             </v-layout>
+                      ></v-select>
 
-             <v-layout> 
-               <v-flex md12 xs12> 
 
-                    <input type="file" name="upload" id="upload" accept="application/pdf" @change="uploadFile">
-               </v-flex>
-             </v-layout>
-           </v-container>
+                	<h1 class="display-3 my-2" v-if="!editing.title"  @click="editing.title = true">{{ submission.title }}</h1>
+                    <v-text-field v-else autofocus v-model="submission.title" outline size="24px"  @blur="editing.title = false"></v-text-field>
 
-   </div>
-   </form>
+
+                	<v-list-tile>
+                		<v-list-tile-avatar>
+                			<v-img
+                     	   src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                     	 >	</v-img>
+                     	</v-list-tile-avatar>
+                     	 <v-list-tile-content>
+                     	 	<h3>Emeka Obi</h3>
+                        <span>{{ today }}</span>
+                     	 </v-list-tile-content>
+                	</v-list-tile>
+                      
+
+                </header>
+                <section class="section-across">
+                	<v-img
+                     	src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                     	>
+                     </v-img>
+
+                </section>
+
+	            <article>
+                 
+                 <blockquote>
+                   <p v-if="!editing.intro_text"  v-html="submission.intro_text" @click="editing.intro_text = true"> </p>
+                   <v-textarea outline row="20" cols="20" autofocus v-else v-model="submission.intro_text"  @blur="editing.intro_text = false"></v-textarea>
+                 </blockquote>
+                 
+
+	                
+	               <p v-if="!editing.fulltext"  v-html="submission.fulltext" @click="editing.fulltext = true"> </p>
+                 <vue-editor autofocus v-else v-model="submission.fulltext"  @blur="editing.fulltext = false"></vue-editor>
+
+                <!--    <v-textarea outline row="20" cols="20" autofocus v-else v-model="submission.fulltext"  @blur="editing.fulltext = false"></v-textarea> -->
+
+                     <author-bio-edit/>
+	            </article>
+
+	  		</v-flex>
+	  		
+	  	</v-layout>
+	   </v-container>
   </div>
 </template>
 <script>
-// import AuthorBioEdit from '~/components/dashboard/AuthorBioEdit.vue'
+import { VueEditor } from "vue2-editor"
+import AuthorBioEdit from '~/components/dashboard/AuthorBioEdit.vue'
 export default {
 
 // middleware: ['middleware'],
@@ -57,17 +88,16 @@ export default {
 layout: 'default',
 
 
-// components: { AuthorBioEdit }, 
+components: { AuthorBioEdit, VueEditor}, 
  data(){
  	  return {
         
-        submission: {
+        submission: this.$store.getters['dashboard/getSubmission'] || {
                      category_id: null,
                      title: "Click here to enter Title ",
                      fulltext: `Click here to Write the full story`,
                      intro_text: "Click here to Write the summary",
-                     images: "",
-                     attachment: null
+                     images: ""
                     },
 
  	  	  editing: {
@@ -78,8 +108,7 @@ layout: 'default',
  	  	  	  images: false
 
  	  	  },
-       fileUrl: null,
-       fileBlob: null
+
  	  }
  },
 
@@ -88,7 +117,7 @@ async fetch({store,params}){
    
      await store.dispatch("dashboard/retrieveSubmittableCategories")
      if(params.submission !== 'new'){
-         
+ 
         await store.dispatch("dashboard/retrieveSubmission",{slug: params.submission})
         
      }
@@ -149,73 +178,13 @@ computed:{
 
 },
 methods:{
-
-      uploadFile(e){
-        const files = e.target.files
-         if(files[0] !== undefined) {
-           
-           var mime = files[0].type
-           // console.log(files[0])
-           const fr = new FileReader ()
-           fr.readAsDataURL(files[0])
-        
-             fr.addEventListener('load', () => {
-                  this.fileUrl = fr.result
-                  
-                  // console.log(fr.result)
-
-                  this.submission.attachment = this.convertFileToBlob(fr.result,mime)
-
-                  // console.log(this.fileBlob)
-               })
-           }
-      },
      
-      convertFileToBlob(fileBase64,mimeType){
-
-        
-       let base64FileContent  =  fileBase64.replace(/^data:application\/(pdf|doc|docx);base64,/ ,"")
-       let mime = mimeType || ''
-       let sliceSize = 1024
-      // console.log(base64FileContent)
-       let byteChars = window.atob(base64FileContent)
-       // console.log(byteChars)
-       let byteArrays  = []
-
-       for( var offset = 0, len = byteChars.length; offset < len; offset += sliceSize){
-            var slice = byteChars.slice(offset, offset + sliceSize)
-
-            var byteNumbers = new Array(slice.length)
-            for( var i = 0; i < slice.length; i++){
-                byteNumbers[i] = slice.charCodeAt(i)
-            }
-            var byteArray = new Uint8Array(byteNumbers)
-            
-            byteArrays.push(byteArray)
-       }
-           
-           
-            return new Blob(byteArrays, {type: mime})
-         
-    },
-
-
      	save(){
      		  
           let slug = this.$route.params.submission
           let authorSlug = this.$route.params.author
-          
-          let data = new FormData()
-          
-
-          for (const [key, value ] of Object.entries(this.submission)) {
-                 console.log(key,value)
-                 data.append(key,value)
-
-          }
-
-
-     		  this.$store.dispatch('dashboard/makeSubmission',data)
+         
+     		  this.$store.dispatch('dashboard/makeSubmission',this.submission)
             .then((response) => {
 
                 slug  =  response.data.slug
@@ -229,15 +198,6 @@ methods:{
           
 
      	},
-      withdraw(){
-        // alert("Withdrawing submission:  Not Implemented")
-        var slug = this.$route.params.submission
-        if(confirm("This will delete this submission irreversibly. Do you want to continue ?")){
-
-             this.$store.dispatch("dashboard/withdrawSubmission",{slug: slug})
-        }
-        
-    },
 
      	discard(){
           this.editingValues(false)
@@ -248,16 +208,7 @@ methods:{
      	submit(){
      		  this.editingValues(false)
           let authorslug = this.$route.params.author
-
-          let data = new FormData()
-          
-
-          for (const [key, value ] of Object.entries(this.submission)) {
-                 console.log(key,value)
-                 data.append(key,value)
-
-          }
-     		  this.$store.dispatch('dashboard/makeSubmission',data)
+     		  this.$store.dispatch('dashboard/makeSubmission',this.submission)
 
           this.$router.push({path: `/dashboard/${authorslug}`})
 
